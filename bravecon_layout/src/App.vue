f84244021157968bb8450b9219c98296e8384fab
dsc <974904977@qq.com>
2025-01-21 22:28:59 +0800

    1

--- bravecon_layout/src/App.vue
+++ bravecon_layout/src/App.vue
@@ -1,9 +1,50 @@
 <template>
-  <nav>
-  </nav>
-  <router-view/>
+  <div id="app" v-title data-title="后台管理系统">
+    <router-view v-if="isRouterAlive"/>
+  </div>
 </template>
 
 <style>
+*{
+  font-family:Helvetica, Tahoma, Arial;
+}
+*::selection {
+  background:#ccc;
+}
+*::-moz-selection {
+  background:#ccc;
+}
+html, body, #app {
+  margin: 0;
+  padding: 0;
+}
+.el-header,.el-main,.el-footer{
+  padding: 0px !important;
+}
 
+.el-radio__input.is-checked .el-radio__inner {
+  border-color: #767676;
+  background: #767676;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      isRouterAlive: true
+    };
+  },
+  methods: {
+
+  }
+}
+
+
+</script>
